f70eab3a1d089aeba120cd09c57862a406e05be7
Tom <thomasdhawkins@eaton.com>
2010-04-17 23:41:00 +0000

    initialize signals at definition

--- Data/VCD.hs
+++ Data/VCD.hs
@@ -17,19 +17,21 @@ import System.IO
 import Text.Printf
 
 data VCD = VCD
-  { handle :: Handle
-  , defs   :: IORef Bool
-  , dirty  :: IORef Bool
-  , time   :: IORef Int
-  , codes  :: IORef [String]
+  { handle   :: Handle
+  , defs     :: IORef Bool
+  , dirty    :: IORef Bool
+  , time     :: IORef Int
+  , codes    :: IORef [String]
+  , dumpvars :: IORef (IO ())
   }
 
-data Timescale = US | MS | S
+data Timescale = S | MS | US | PS
 
 instance Show Timescale where
-  show US = "us"
-  show MS = "ms"
   show S  = "s"
+  show MS = "ms"
+  show US = "us"
+  show PS = "ps"
 
 assertDefs :: VCD -> IO ()
 assertDefs vcd = do
@@ -48,76 +50,38 @@ nextCode vcd = do
   writeIORef (codes vcd) (tail codes')
   return $ head codes'
 
-class Signal a where signal :: VCD -> String -> IO (a -> IO ())
-
-instance Signal Bool where
-  signal vcd name = do
-    code <- nextCode vcd
-    hPutStrLn (handle vcd) $ printf "$var wire 1 %s %s $end" code name
-    last <- newIORef Nothing
-    return $ \ a -> do
-      assertNotDefs vcd
-      last' <- readIORef last
-      when (last' /= Just a) $ do
-        hPutStrLn (handle vcd) $ (if a then "1" else "0") ++ code
-        writeIORef last $ Just a
-        writeIORef (dirty vcd) True
-
-bitsSignal :: Bits a => VCD -> String -> IO (a -> IO ())
-bitsSignal vcd name = do
-  let dumb = 0
-  code <- nextCode vcd
-  hPutStrLn (handle vcd) $ printf "$var %s %d %s %s $end" (if isSigned dumb then "integer" else "wire") (bitSize dumb) code name
-  last <- newIORef Nothing
-  return $ \ a -> do
-    assertNotDefs vcd
-    when (a == dumb) (return ())
-    last' <- readIORef last
-    when (last' /= Just a) $ do
-      hPutStrLn (handle vcd) $ printf "b%s %s" (bitString a) code
-      writeIORef last $ Just a
-      writeIORef (dirty vcd) True
+class    Signal a      where signal :: VCD -> String -> a -> IO (a -> IO ())
+instance Signal Bool   where signal = package "wire" 1 (\ a -> if a then "1" else "0")
+instance Signal Int    where signal vcd name init = package "integer" (bitSize init) bitString vcd name init
+instance Signal Int8   where signal = package "integer"  8 bitString
+instance Signal Int16  where signal = package "integer" 16 bitString
+instance Signal Int32  where signal = package "integer" 16 bitString
+instance Signal Int64  where signal = package "integer" 16 bitString
+instance Signal Word8  where signal = package "wire"     8 bitString
+instance Signal Word16 where signal = package "wire"    16 bitString
+instance Signal Word32 where signal = package "wire"    32 bitString
+instance Signal Word64 where signal = package "wire"    64 bitString
+instance Signal Float  where signal = package "real" 32 (\ a -> "r" ++ show a ++ " ")
+instance Signal Double where signal = package "real" 64 (\ a -> "r" ++ show a ++ " ")
 
 bitString :: Bits a => a -> String
-bitString n = if null bits then "0" else bits
+bitString n = "b" ++ (if null bits then "0" else bits) ++ " "
   where
   bits = dropWhile (== '0') $ [ if testBit n i then '1' else '0' | i <- [bitSize n - 1, bitSize n - 2 .. 0] ]
 
-instance Signal Int    where signal = bitsSignal
-instance Signal Int8   where signal = bitsSignal
-instance Signal Int16  where signal = bitsSignal
-instance Signal Int32  where signal = bitsSignal
-instance Signal Int64  where signal = bitsSignal
-instance Signal Word8  where signal = bitsSignal
-instance Signal Word16 where signal = bitsSignal
-instance Signal Word32 where signal = bitsSignal
-instance Signal Word64 where signal = bitsSignal
-   
-instance Signal Float where
-  signal vcd name = do
-    code <- nextCode vcd
-    hPutStrLn (handle vcd) $ printf "$var real 32 %s %s $end" code name
-    last <- newIORef Nothing
-    return $ \ a -> do
-      assertNotDefs vcd
-      last' <- readIORef last
-      when (last' /= Just a) $ do
-        hPutStrLn (handle vcd) $ printf "r%s %s" (show a) code
-        writeIORef last $ Just a
-        writeIORef (dirty vcd) True
-
-instance Signal Double where
-  signal vcd name = do
-    code <- nextCode vcd
-    hPutStrLn (handle vcd) $ printf "$var real 64 %s %s $end" code name
-    last <- newIORef Nothing
-    return $ \ a -> do
-      assertNotDefs vcd
-      last' <- readIORef last
-      when (last' /= Just a) $ do
-        hPutStrLn (handle vcd) $ printf "r%s %s" (show a) code
-        writeIORef last $ Just a
-        writeIORef (dirty vcd) True
+package :: Eq a => String -> Int -> (a -> String) -> VCD -> String -> a -> IO (a -> IO ())
+package typ width value vcd name init = do
+  code <- nextCode vcd
+  hPutStrLn (handle vcd) $ printf "$var %s %d %s %s $end" typ width code name
+  last <- newIORef Nothing
+  let sample a = do assertNotDefs vcd
+                    last' <- readIORef last
+                    when (last' /= Just a) $ do
+                      hPutStrLn (handle vcd) $ value a ++ code
+                      writeIORef last $ Just a
+                      writeIORef (dirty vcd) True
+  modifyIORef (dumpvars vcd) (\ a -> a >> sample init)
+  return sample
 
 
 newVCD :: Handle -> Timescale -> IO VCD
@@ -125,16 +89,18 @@ newVCD h ts = do
   hPutStrLn h $ "$timescale"
   hPutStrLn h $ "  1 " ++ show ts
   hPutStrLn h $ "$end"
-  defs  <- newIORef True
-  dirty <- newIORef True
-  time  <- newIORef 0
-  codes <- newIORef identCodes
+  defs     <- newIORef True
+  dirty    <- newIORef True
+  time     <- newIORef 0
+  codes    <- newIORef identCodes
+  dumpvars <- newIORef $ return ()
   return VCD
-    { handle = h
-    , defs   = defs
-    , dirty  = dirty
-    , time   = time
-    , codes  = codes
+    { handle   = h
+    , defs     = defs
+    , dirty    = dirty
+    , time     = time
+    , codes    = codes
+    , dumpvars = dumpvars
     }
 
 level :: VCD -> String -> IO a -> IO a
@@ -146,10 +112,14 @@ level vcd name a = do
 
 step :: VCD -> Int -> IO ()
 step vcd n = do
-  defs' <- readIORef $ defs vcd
+  defs'     <- readIORef $ defs     vcd
+  dumpvars' <- readIORef $ dumpvars vcd
   when defs' $ do
-    hPutStrLn (handle vcd) "$enddefinitions $end"
     writeIORef (defs vcd) False
+    hPutStrLn (handle vcd) "$enddefinitions $end"
+    hPutStrLn (handle vcd) "$dumpvars"
+    dumpvars'
+    hPutStrLn (handle vcd) "$end"
 
   dirty' <- readIORef $ dirty vcd
   when dirty' $ do
